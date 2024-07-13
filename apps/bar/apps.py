5141d351d13e1034f407c0affe451297d305e4fa
Aidarkg <adijamgyrchiev@gmail.com>
2024-07-13 20:16:36 +0600

    удалил файлы

--- apps/bar/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class BarConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.bar'
-
-    verbose_name = 'Бар'
-    verbose_name_plural = 'Бар'
