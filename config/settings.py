afd4cf2b0a21de01a50dff850bcad94da281d51a
Aidarkg <adijamgyrchiev@gmail.com>
2024-07-13 20:34:18 +0600

    ref debug

--- config/settings.py
+++ config/settings.py
@@ -7,7 +7,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = True
+if config('DEBUG') == 'True':
+    DEBUG = True
+else:
+    DEBUG = False
+
 
 ALLOWED_HOSTS = ['*']
 
